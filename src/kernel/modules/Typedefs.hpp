1c778e0e882d160eaa0f8703be3b3e88fc470e49
Blackneeeed <dodatkowekonto.marcelclay@gmail.com>
2024-03-05 21:24:45 +0100

    Delete include guards, update to #pragma once

--- src/kernel/modules/Typedefs.hpp
+++ src/kernel/modules/Typedefs.hpp
@@ -1,11 +1,8 @@
 #pragma once
 
-#ifndef TYPEDEFS_HPP_
-#define TYPEDEFS_HPP_
 typedef unsigned long long size_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 #define NULL nullptr;
-#endif
