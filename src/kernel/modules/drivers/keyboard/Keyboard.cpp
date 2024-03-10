0d6c1b9488d9c95ff57fcf7ebadae23340dddf66
Blackneeeed <dodatkowekonto.marcelclay@gmail.com>
2024-03-10 21:48:14 +0100

    ...

--- src/kernel/modules/drivers/keyboard/Keyboard.cpp
+++ src/kernel/modules/drivers/keyboard/Keyboard.cpp
@@ -1,13 +1,16 @@
 #pragma once
 #include "../../commands.cpp"
 #include "../../Typedefs.hpp"
-#include "KeyboardDriver.cpp"
+#include "Keys.hpp"
+#include "Keymap.hpp"
+#include "../debug/E9.cpp"
 
 #define TAB_WIDTH 4
 #define MAX_COMMAND_LENGTH 128
 char commandBuffer[MAX_COMMAND_LENGTH];
 uint8_t commandLength;
 extern uint16_t lastPrint;
+bool capsLockPressed, leftShiftPressed, rightShiftPressed = false;
 
 void handleCharacter(char chr) {
     E9_WriteString("Character '");
@@ -44,67 +47,65 @@ void handleEnter() {
     lastPrint = cursorPos;
 }
 
-KeyboardDriver kbDriver = KeyboardDriver();
 
 extern "C" void isr1_handler()
 {
-    kbDriver.main();
-    // uint8_t scanCode = inb(0x60);
-    // switch (scanCode) {
-    //     case ENTER:
-    //         handleEnter();
-    //         break;
+    uint8_t scanCode = inb(0x60);
+    switch (scanCode) {
+        case ENTER:
+            handleEnter();
+            break;
 
-    //     case BACKSPACE:
-    //         handleBackspace();
-    //         break;
+        case BACKSPACE:
+            handleBackspace();
+            break;
 
-    //     case TAB:
-    //         handleTab();
-    //         break;
+        case TAB:
+            handleTab();
+            break;
 
-    //     case CAPS:
-    //         E9_WriteString("Caps lock pressed\r\n");
-    //         capsLockPressed = !capsLockPressed;
-    //         break;
+        case CAPS:
+            E9_WriteString("Caps lock pressed\r\n");
+            capsLockPressed = !capsLockPressed;
+            break;
 
-    //     case LEFTSHIFT:
-    //         E9_WriteString("LSHIFT pressed\r\n");
-	// 		leftShiftPressed = true;
-	// 		break;
+        case LEFTSHIFT:
+            E9_WriteString("LSHIFT pressed\r\n");
+			leftShiftPressed = true;
+			break;
 
-	// 	case LEFTSHIFTRELEASE:
-	// 		E9_WriteString("LSHIFT released\r\n");
-    //         leftShiftPressed = false;
-    //         break;
+		case LEFTSHIFTRELEASE:
+			E9_WriteString("LSHIFT released\r\n");
+            leftShiftPressed = false;
+            break;
         
-	// 	case RIGHTSHIFT:
-	// 		E9_WriteString("RSHIFT pressed\r\n");
-	// 		rightShiftPressed = true;
-	// 		break;
-	// 	case RIGHTSHIFTRELEASE:
-	// 		E9_WriteString("RSHIFT released\r\n");
-	// 		rightShiftPressed = false;
-	// 		break;
+		case RIGHTSHIFT:
+			E9_WriteString("RSHIFT pressed\r\n");
+			rightShiftPressed = true;
+			break;
+		case RIGHTSHIFTRELEASE:
+			E9_WriteString("RSHIFT released\r\n");
+			rightShiftPressed = false;
+			break;
 
 
-    //     default:
-    //         if (scanCode < 0x3A)
-    //         {
-    //             if (leftShiftPressed | rightShiftPressed) {
-    //                 if (!capsLockPressed) {
-    //                     handleCharacter(charToUpper(KBset1::ScanCodeLookupTableShift[scanCode]));
-    //                 } else {
-    //                     handleCharacter(charToLower(KBset1::ScanCodeLookupTableShift[scanCode]));
-    //                 }
-    //             } else if (capsLockPressed) {
-    //                 handleCharacter(charToUpper(KBset1::ScanCodeLookupTable[scanCode]));
-    //             } else {
-    //                 handleCharacter(charToLower(KBset1::ScanCodeLookupTable[scanCode]));
-    //             }
-    //         }
-    //         break;
-    // }
+        default:
+            if (scanCode < 0x3A)
+            {
+                if (leftShiftPressed | rightShiftPressed) {
+                    if (!capsLockPressed) {
+                        handleCharacter(charToUpper(KBset1::ScanCodeLookupTableShift[scanCode]));
+                    } else {
+                        handleCharacter(charToLower(KBset1::ScanCodeLookupTableShift[scanCode]));
+                    }
+                } else if (capsLockPressed) {
+                    handleCharacter(charToUpper(KBset1::ScanCodeLookupTable[scanCode]));
+                } else {
+                    handleCharacter(charToLower(KBset1::ScanCodeLookupTable[scanCode]));
+                }
+            }
+            break;
+    }
     outb(0x20, 0x20);
     outb(0xa0, 0x20);
 }
